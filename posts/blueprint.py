e2451ef276ec184939d80c42eec8e9dadb66ef03
kotanskiy <olexiyyefimenko@gmail.com>
2018-05-26 18:36:02 +0300

    form for creation posts

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,13 +1,45 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from models import Post, Tag
+from .forms import PostForm
+from app import database
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            database.session.add(post)
+            database.session.commit()
+        except:
+            print('Something wrong')
+        return redirect(url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
-    return render_template('posts/index.html', posts=posts)
+    search_value = request.args.get('search')
+    page = request.args.get('page')
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
+    if search_value:
+        posts = Post.query.filter(Post.title.contains(search_value) | Post.body.contains(search_value))
+    else:
+        posts = Post.query.order_by(Post.created.desc())
+
+    pages = posts.paginate(page=page, per_page=5)
+    return render_template('posts/index.html', posts=posts, pages=pages)
 
 
 @posts.route('/<slug>')
